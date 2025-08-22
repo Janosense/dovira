e08874f08589469fea6f5492aab524717059d167
Tymofii Synianskyi <janosense@gmail.com>
2025-08-22 17:02:48 +0300

    Add custom 404 page with styling and disable WP_DEBUG mode

---
+++ wp-content/themes/dovira/404.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+<main>
+	<div class="not-found">
+		<div class="wrapper not-found__wrapper">
+			<p class="not-found__message">404<br>
+				<span>Сторінку не знайдено</span></p>
+			<ul class="not-found__list">
+				<li>
+					<a href="/"></a>Головна
+				</li>
+				<li>
+					<a href="/services/"></a>Послуги
+				</li>
+				<li>
+					<a href="/contacts/"></a>Контакти
+				</li>
+			</ul>
+		</div>
+	</div>
+
+</main>
+<?php get_footer(); ?>
