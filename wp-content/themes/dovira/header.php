704c59194b210f7bb8a11e7ac0ec221adfd0c7fd
Tymofii Synianskyi <janosense@gmail.com>
2025-09-05 11:46:45 +0300

    Add Google Tag Manager integration with script in header and noscript fallback in body for Dovira theme

--- wp-content/themes/dovira/header.php
+++ wp-content/themes/dovira/header.php
@@ -18,7 +18,25 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Oswald:wght@200..700&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Oswald:wght@200..700&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+		rel="stylesheet">
+
+	<!-- Google Tag Manager -->
+	<script>(function (w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start':
+					new Date().getTime(), event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-NP5JF53Z');</script>
+	<!-- End Google Tag Manager -->
 
 	<?php wp_head(); ?>
 	<link rel="icon" type="image/png" href="/wp-content/themes/dovira/source/images/favicon/favicon-96x96.png?v=1.0.2"
@@ -31,5 +49,11 @@
 	<link rel="manifest" href="/wp-content/themes/dovira/source/images/favicon/site.webmanifest?v=1.0.2"/>
 </head>
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+	<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NP5JF53Z"
+			height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php wp_body_open(); ?>
 <?php get_template_part( 'template-parts/header/site-header' ); ?>
