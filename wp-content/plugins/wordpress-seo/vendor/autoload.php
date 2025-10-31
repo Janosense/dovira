ae799fae7d8d9f213d078e07f10a2775796ec12f
Tymofii Synianskyi <janosense@gmail.com>
2025-10-31 17:50:33 +0200

    Updated plugins

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit91347d4778f0a8704858a500d0acfeb6::getLoader();
+return ComposerAutoloaderInita55712f227f685a57099bd044b4ce723::getLoader();
