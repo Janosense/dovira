2293e7aec3685da6fd1a3cbc60244e9cb795c2fb
Tymofii Synianskyi <janosense@gmail.com>
2025-11-12 16:42:27 +0200

    Refactor: replace `toggleEmployeesCity` with `toggleCities` for improved modularity and simplified DOM structure handling.

--- wp-content/themes/dovira/source/scripts/app.js
+++ wp-content/themes/dovira/source/scripts/app.js
@@ -2,14 +2,11 @@
   const {initAnimations} = await import('@scripts/modules/init-animations');
   const {initFancybox} = await import('@scripts/modules/init-fancybox');
   const {toggleAccordion} = await import('@scripts/modules/toggle-accordion');
-  const {checkTest} = await import('@scripts/modules/check-test');
-  const {authentication} = await import('@scripts/modules/authentication');
-  const {resetProgress} = await import('@scripts/modules/reset-progress');
   const {toggleMobileNav} = await import('@scripts/modules/toggle-mobile-nav');
   const {servicesSearch} = await import('@scripts/modules/services-search');
   const {initServicePriceLists} = await import('@scripts/modules/init-service-price-lists');
   const {initSwiper} = await import('@scripts/modules/init-swiper');
-  const {toggleEmployeesCity} = await import('@scripts/modules/toggle-employees-city');
+  const {toggleCities} = await import('@scripts/modules/toggle-cities.js');
   const {toggleFooterCalls} = await import('@scripts/modules/toggle-footer-calls');
   const {questionaryFormHandler} = await import('@scripts/modules/questionary-form-handler.js');
   const {initImask} = await import('@scripts/modules/init-imask.js');
@@ -17,14 +14,11 @@
   initAnimations();
   initFancybox();
   toggleAccordion();
-  checkTest();
-  authentication();
-  resetProgress();
   toggleMobileNav();
   servicesSearch();
   initServicePriceLists();
   initSwiper()
-  toggleEmployeesCity();
+  toggleCities();
   toggleFooterCalls();
   questionaryFormHandler();
   initImask()
---
+++ wp-content/themes/dovira/source/scripts/modules/toggle-cities.js
@@ -0,0 +1,30 @@
+const toggleCities = () => {
+  const cityToggles = document.querySelectorAll('.city-toggle');
+
+  if (cityToggles.length > 0) {
+
+    cityToggles.forEach((toggle, index) => {
+      const items = toggle.nextElementSibling.querySelectorAll('li');
+
+      toggle.addEventListener('click', (evt) => {
+        if (evt.target.dataset.city) {
+          toggle.querySelectorAll('button').forEach((button) => {
+            button.classList.remove('city-toggle__button--active');
+            evt.target.classList.add('city-toggle__button--active');
+          });
+
+          let activeCity = evt.target.dataset.city;
+          items.forEach((item) => {
+            if (item.dataset.city.indexOf(activeCity) !== -1) {
+              item.style.display = 'block';
+            } else {
+              item.style.display = 'none';
+            }
+          })
+        }
+      })
+    });
+  }
+}
+
+export {toggleCities};
