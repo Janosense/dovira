889ca14cd122a3e7f5d5e101a598c6428bb28d5f
Tymofii Synianskyi <janosense@gmail.com>
2025-12-10 14:25:33 +0200

    Improve "Dovira" theme: add null check for city toggle sibling to prevent errors.

--- wp-content/themes/dovira/source/scripts/modules/toggle-cities.js
+++ wp-content/themes/dovira/source/scripts/modules/toggle-cities.js
@@ -4,25 +4,27 @@ const toggleCities = () => {
   if (cityToggles.length > 0) {
 
     cityToggles.forEach((toggle, index) => {
-      const items = toggle.nextElementSibling.querySelectorAll('li');
+      if (toggle.nextElementSibling) {
+        const items = toggle.nextElementSibling.querySelectorAll('li');
 
-      toggle.addEventListener('click', (evt) => {
-        if (evt.target.dataset.city) {
-          toggle.querySelectorAll('button').forEach((button) => {
-            button.classList.remove('city-toggle__button--active');
-            evt.target.classList.add('city-toggle__button--active');
-          });
+        toggle.addEventListener('click', (evt) => {
+          if (evt.target.dataset.city) {
+            toggle.querySelectorAll('button').forEach((button) => {
+              button.classList.remove('city-toggle__button--active');
+              evt.target.classList.add('city-toggle__button--active');
+            });
 
-          let activeCity = evt.target.dataset.city;
-          items.forEach((item) => {
-            if (item.dataset.city.indexOf(activeCity) !== -1) {
-              item.style.display = 'block';
-            } else {
-              item.style.display = 'none';
-            }
-          })
-        }
-      })
+            let activeCity = evt.target.dataset.city;
+            items.forEach((item) => {
+              if (item.dataset.city.indexOf(activeCity) !== -1) {
+                item.style.display = 'block';
+              } else {
+                item.style.display = 'none';
+              }
+            })
+          }
+        })
+      }
     });
   }
 }
