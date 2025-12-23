3633d971857e07d5260812927014b759b839e398
Tymofii Synianskyi <janosense@gmail.com>
2025-12-23 13:04:08 +0200

    Add "Scroll to top" button with functionality and corresponding CSS, enhance footer interaction logic, and refine SVG markup styling.

--- wp-content/themes/dovira/source/scripts/modules/toggle-footer-calls.js
+++ wp-content/themes/dovira/source/scripts/modules/toggle-footer-calls.js
@@ -12,11 +12,22 @@ const initTogglePhones = () => {
 
 const toggleFooterCalls = () => {
   const footerCalls = document.querySelector('.footer-calls');
+  const buttonUp = document.querySelector('.up-button');
   if (footerCalls) {
     initTogglePhones();
     const toggle = footerCalls.querySelector('.footer-calls__toggle');
     toggle.addEventListener('click', (evt) => {
       footerCalls.classList.toggle('footer-calls--active');
+
+      if (buttonUp) {
+        if (footerCalls.classList.contains('footer-calls--active')) {
+          buttonUp.classList.remove('up-button--active');
+        } else if (!footerCalls.classList.contains('footer-calls--active') && window.scrollY > 300) {
+          setTimeout(() => {
+            buttonUp.classList.add('up-button--active');
+          }, 500)
+        }
+      }
     });
   }
 }
--- wp-content/themes/dovira/template-parts/footer/site-footer.php
+++ wp-content/themes/dovira/template-parts/footer/site-footer.php
@@ -61,14 +61,19 @@ $social_links = dovira_get_acf_field( 'social_links', 'option' );
 								<?php endforeach; ?>
 							<?php endif; ?>
 							<?php if ( ! empty( $contact['email'] ) ) : ?>
-								<a href="mailto:<?= antispambot( $contact['email'] ); ?>" class="footer__address-email"><?= antispambot( $contact['email'] ); ?></a>
+								<a href="mailto:<?= antispambot( $contact['email'] ); ?>"
+								   class="footer__address-email"><?= antispambot( $contact['email'] ); ?></a>
 							<?php endif; ?>
 							<?php if ( ! empty( $contact['instagram_link'] ) ) : ?>
-								<a href="<?= $contact['instagram_link']; ?>" class="footer__address-instagram" target="_blank">
+								<a href="<?= $contact['instagram_link']; ?>" class="footer__address-instagram"
+								   target="_blank">
 									<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none">
-										<path fill-rule="evenodd" clip-rule="evenodd" d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z" />
-										<path d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z" />
-										<path fill-rule="evenodd" clip-rule="evenodd" d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z" />
+										<path fill-rule="evenodd" clip-rule="evenodd"
+											  d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"/>
+										<path
+											d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"/>
+										<path fill-rule="evenodd" clip-rule="evenodd"
+											  d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z"/>
 									</svg>
 									Instagram
 								</a>
@@ -86,7 +91,14 @@ $social_links = dovira_get_acf_field( 'social_links', 'option' );
 			</div>
 		</div>
 	</div>
-
+	<button class="up-button" type="button" aria-label="Scroll up">
+		<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+			 viewBox="0 0 32 32" enable-background="new 0 0 32 32" xml:space="preserve">
+			<line fill="none" stroke="#FFFFFF" stroke-width="2" stroke-miterlimit="10" x1="16" y1="6" x2="16" y2="28"/>
+			<polyline fill="none" stroke="#FFFFFF" stroke-width="2" stroke-miterlimit="10"
+					  points="7.5,14 16,5.5 24.5,14 "/>
+		</svg>
+	</button>
 	<?php
 	$main_phone = dovira_get_acf_field( 'contacts_main_phone', 'option' );
 	$telegram   = dovira_get_acf_field( 'contacts_telegram', 'option' );
@@ -147,4 +159,33 @@ $social_links = dovira_get_acf_field( 'social_links', 'option' );
 		</div>
 	<?php endif; ?>
 </footer>
+<script>
+	document.addEventListener('DOMContentLoaded', function () {
+		const buttonUp = document.querySelector('.up-button');
+		const footerCalls = document.querySelector('.footer-calls');
+
+		if (buttonUp) {
+			function checkViewport() {
+
+				if (window.scrollY > 300 && !footerCalls.classList.contains('footer-calls--active')) {
+					buttonUp.classList.add('up-button--active');
+				} else {
+					buttonUp.classList.remove('up-button--active');
+				}
+			}
+
+			buttonUp.addEventListener('click', function () {
+				document.documentElement.scrollTo({
+					top: 0,
+					left: 0,
+					behavior: "smooth",
+				});
+			})
+
+
+			window.addEventListener('scroll', checkViewport);
+			checkViewport();
+		}
+	});
+</script>
 
