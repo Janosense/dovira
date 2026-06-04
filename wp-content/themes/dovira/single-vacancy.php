85c087f05daac9eb186ed886f283d33b8d10f0f5
Tymofii Synianskyi <janosense@gmail.com>
2026-06-04 17:25:39 +0300

    Update "Dovira" theme: adjust vacancy contact forms for Polylang language support and fix SVG formatting.

--- wp-content/themes/dovira/single-vacancy.php
+++ wp-content/themes/dovira/single-vacancy.php
@@ -35,10 +35,10 @@
 							<?php foreach ( $cities as $city ): ?>
 								<div class="vacancy__location">
 									<svg width="20" height="20" viewBox="0 0 20 20" fill="none"
-										 xmlns="http://www.w3.org/2000/svg">
+									     xmlns="http://www.w3.org/2000/svg">
 										<path opacity="0.4"
-											  d="M17.1829 7.04183C16.3079 3.19183 12.9496 1.4585 9.99959 1.4585C9.99959 1.4585 9.99959 1.4585 9.99126 1.4585C7.04959 1.4585 3.68292 3.1835 2.80792 7.0335C1.83292 11.3335 4.46626 14.9752 6.84959 17.2668C7.73292 18.1168 8.86626 18.5418 9.99959 18.5418C11.1329 18.5418 12.2663 18.1168 13.1413 17.2668C15.5246 14.9752 18.1579 11.3418 17.1829 7.04183Z"
-											  fill="#C06F94"/>
+										      d="M17.1829 7.04183C16.3079 3.19183 12.9496 1.4585 9.99959 1.4585C9.99959 1.4585 9.99959 1.4585 9.99126 1.4585C7.04959 1.4585 3.68292 3.1835 2.80792 7.0335C1.83292 11.3335 4.46626 14.9752 6.84959 17.2668C7.73292 18.1168 8.86626 18.5418 9.99959 18.5418C11.1329 18.5418 12.2663 18.1168 13.1413 17.2668C15.5246 14.9752 18.1579 11.3418 17.1829 7.04183Z"
+										      fill="#C06F94"/>
 										<path
 											d="M10 11.2168C11.4497 11.2168 12.625 10.0415 12.625 8.5918C12.625 7.14205 11.4497 5.9668 10 5.9668C8.55025 5.9668 7.375 7.14205 7.375 8.5918C7.375 10.0415 8.55025 11.2168 10 11.2168Z"
 											fill="#C06F94"/>
@@ -132,8 +132,13 @@
 							<h3 class="heading heading--h5 contacts-form7__heading">Надіслати резюме:</h3>
 							<?php if ( getenv( 'IS_DDEV_PROJECT' ) == 'true' ) : ?>
 								<?= do_shortcode( '[contact-form-7 id="1321657" title="Форма заявки на вакансію" vacancy="' . $post->ID . '"]' ); ?>
-							<?php else : ?>
-								<?= do_shortcode( '[contact-form-7 id="f8f78c9" title="Форма заявки на вакансію" vacancy="' . $post->ID . '"]' ); ?>
+							<?php else :
+								$current_language_slug = pll_current_language( 'slug' );
+								if ( $current_language_slug === 'uk' ) :?>
+									<?= do_shortcode( '[contact-form-7 id="f8f78c9" title="Форма заявки на вакансію" vacancy="' . $post->ID . '"]' ); ?>
+								<?php elseif ( $current_language_slug === 'ru' ) : ?>
+									<?= do_shortcode( '[contact-form-7 id="48d74aa" title="Форма заявки на вакансію RU" vacancy="' . $post->ID . '"]' ); ?>
+								<?php endif; ?>
 							<?php endif; ?>
 							<div class="contacts-form7__google-disclaimer">
 								Цей сайт захищено reCAPTCHA, а також застосовуються <a
