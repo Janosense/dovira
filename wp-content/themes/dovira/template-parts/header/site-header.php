cfcba55d8a72674731f8770f20e8307dbed44f1d
Tymofii Synianskyi <janosense@gmail.com>
2026-09-01 10:43:43 +0300

    Update "Dovira" theme: add location switcher for Kyiv and Kharkiv, integrate multilingual support, and include responsive styles for mobile and desktop

--- wp-content/themes/dovira/template-parts/header/site-header.php
+++ wp-content/themes/dovira/template-parts/header/site-header.php
@@ -1,3 +1,4 @@
+<?php $current_language_slug = pll_current_language( 'slug' ); ?>
 <header class="header">
 	<div class="wrapper header__wrapper">
 		<a href="<?= home_url(); ?>" class="header__logo">
@@ -23,6 +24,24 @@
 				<?php endforeach; ?>
 			</ul>
 		<?php endif; ?>
+		<ul class="location-switcher location-switcher--desktop">
+			<li class="location-switcher__item">
+				<a href="https://kyiv.dovira.vet" class="location-switcher__link"><?= $current_language_slug === 'uk' ? 'Київ' : 'Киев'; ?></a>
+			</li>
+			<li class="location-switcher__item">
+				<div class="location-switcher__link location-switcher__link--active">
+					<svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z"
+							stroke="#c06f94" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+						<path
+							d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z"
+							stroke="#c06f94" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+					</svg>
+					<?= $current_language_slug === 'uk' ? 'Харків' : 'Харьков'; ?>
+				</div>
+			</li>
+		</ul>
 		<ul class="language-switcher language-switcher--desktop">
 			<?php pll_the_languages( [
 				'show_flags'       => 0,
@@ -91,6 +110,24 @@
 			'show_names'       => 1,
 			'display_names_as' => 'name',
 		] ); ?>
+	</ul>
+	<ul class="location-switcher location-switcher--mobile">
+		<li class="location-switcher__item">
+			<a href="https://kyiv.dovira.vet" class="location-switcher__link"><?= $current_language_slug === 'uk' ? 'Київ' : 'Киев'; ?></a>
+		</li>
+		<li class="location-switcher__item">
+			<div class="location-switcher__link location-switcher__link--active">
+				<svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path
+						d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z"
+						stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+					<path
+						d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z"
+						stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+				</svg>
+				<?= $current_language_slug === 'uk' ? 'Харків' : 'Харьков'; ?>
+			</div>
+		</li>
 	</ul>
 	<?php if ( ! empty( $contacts ) ) : ?>
 		<ul class="mobile-nav__phones">
