315b607c39a3f20bf6864d7db28676aaf142d4cf
Tymofii Synianskyi <janosense@gmail.com>
2026-09-21 12:08:14 +0300

    Changed lang attr for RU ver

--- wp-content/themes/dovira/header.php
+++ wp-content/themes/dovira/header.php
@@ -14,7 +14,11 @@
  */
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<?php $lang = get_language_attributes();
+if ( $lang === 'lang="ru-RU"' ) :
+	$lang = 'lang="ru"';
+endif; ?>
+<html <?= $lang; ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
@@ -58,11 +62,11 @@
 
 	<?php wp_head(); ?>
 	<link rel="icon" type="image/png" href="/wp-content/themes/dovira/source/images/favicon/favicon-96x96.png?v=1.0.2"
-	      sizes="96x96"/>
+		  sizes="96x96"/>
 	<link rel="icon" type="image/svg+xml" href="/wp-content/themes/dovira/source/images/favicon/favicon.svg?v=1.0.2"/>
 	<link rel="shortcut icon" href="/wp-content/themes/dovira/source/images/favicon/favicon.ico?v=1.0.2"/>
 	<link rel="apple-touch-icon" sizes="180x180"
-	      href="/wp-content/themes/dovira/source/images/favicon/apple-touch-icon.png?v=1.0.2"/>
+		  href="/wp-content/themes/dovira/source/images/favicon/apple-touch-icon.png?v=1.0.2"/>
 	<meta name="apple-mobile-web-app-title" content="MyWebSite"/>
 	<link rel="manifest" href="/wp-content/themes/dovira/source/images/favicon/site.webmanifest?v=1.0.2"/>
 </head>
